85df182793f6b05e945d3a4b34bd3cf31915a6c5
joshofreeness <joshofreeness@gmail.com>
2014-09-02 15:27:20 +1200

    Trying to properly configure Hibernate

---
+++ workspace/OrderTracking/src/main/java/com/joshofreeness/ordertracking/persistence/HibernateTester.java
@@ -0,0 +1,88 @@
+package com.joshofreeness.ordertracking.persistence;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.joshofreeness.ordertracking.persistence.CustomerDao;
+import com.joshofreeness.ordertracking.persistence.OrderDao;
+import com.joshofreeness.ordertracking.persistence.ProductDao;
+import com.joshofreeness.ordertracking.domain.Customer;
+import com.joshofreeness.ordertracking.domain.Order;
+import com.joshofreeness.ordertracking.domain.Product;
+
+
+public class HibernateTester {
+
+	public static void main(String[] args) {
+		
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load("classpath:app-context.xml");
+		ctx.refresh();
+		
+		CustomerDao customerDao = ctx.getBean("ccustomerDao", CustomerDao.class);
+
+		// List customers without details
+		List<Customer> customers = customerDao.findAll();
+		//listCustomers(customers);	
+		
+		// List customers with details
+		customers = customerDao.findAllWithDetail();
+		listCustomersWithDetail(customers);
+
+		
+		Customer customer;
+		
+		// Find customer by ID
+		customer = customerDao.findById(1l);
+		System.out.println("");
+		System.out.println("Customer with id 1:" + customer);
+		System.out.println("");		
+		
+		// Add new customer
+		customer = new Customer();
+		customer.setFirstName("Michael");
+		customer.setLastName("Jackson");
+		customer.setCredit(200);
+		customerDao.save(customer);
+		customers = customerDao.findAllWithDetail();
+		listCustomersWithDetail(customers);
+
+		// Update customer
+		customer = customerDao.findById(1l);
+		customer.setFirstName("Kim Fung");
+
+		customerDao.save(customer);
+		customers = customerDao.findAllWithDetail();
+		listCustomersWithDetail(customers);
+		
+		// Delete customer
+		customer = customerDao.findById(1l);
+		customerDao.delete(customer);
+		customers = customerDao.findAllWithDetail();
+		listCustomersWithDetail(customers);	
+		
+	}
+	
+	private static void listCustomers(List<Customer> customers) {
+		System.out.println("");
+		System.out.println("Listing customers without details:");
+		for (Customer customer: customers) {
+			System.out.println(customer);		
+			System.out.println();	
+		}
+	}	
+	
+	private static void listCustomersWithDetail(List<Customer> customers) {
+		System.out.println("");
+		System.out.println("Listing customers with details:");
+		for (Customer customer: customers) {
+			System.out.println(customer);
+		
+			System.out.println();
+		}		
+	}	
+	
+}
