7583649e515dca11f9908afa022ef2e5f79239f4
joshofreeness <joshofreeness@gmail.com>
2014-09-03 17:42:34 +1200

    Changed getNamedQueries to createQuery

--- workspace/OrderTracking/src/main/java/com/joshofreeness/ordertracking/persistence/CustomerDaoImpl.java
+++ workspace/OrderTracking/src/main/java/com/joshofreeness/ordertracking/persistence/CustomerDaoImpl.java
@@ -40,12 +40,12 @@ public class CustomerDaoImpl implements CustomerDao{
 
 	public List<Customer> findAllWithDetail() {
 		//TODO: Edit the query so that it joins tables with orders
-		List<Customer> result = sessionFactory.getCurrentSession().getNamedQuery("from Customer c").list();
+		List<Customer> result = sessionFactory.getCurrentSession().createQuery("from Customer c").list();
 		return result;
 	}
 
 	public Customer findById(Long id) {
-		Customer result = (Customer) sessionFactory.getCurrentSession().getNamedQuery("from Customer as c where c.id = :id").setParameter("id", id);
+		Customer result = (Customer) sessionFactory.getCurrentSession().createQuery("from Customer as c where c.id = :id").setParameter("id", id);
 		return result;
 	}
 
