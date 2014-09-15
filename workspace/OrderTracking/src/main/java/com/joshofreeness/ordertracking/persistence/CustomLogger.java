57d2db839712a12a421f8e3bc214f8fbfd407a6a
joshofreeness <joshofreeness@gmail.com>
2014-09-15 14:26:24 +1200

    cleaning

--- workspace/OrderTracking/src/main/java/com/joshofreeness/ordertracking/persistence/CustomLogger.java
+++ workspace/OrderTracking/src/main/java/com/joshofreeness/ordertracking/persistence/CustomLogger.java
@@ -1,19 +1,24 @@
 package com.joshofreeness.ordertracking.persistence;
 
+import java.util.Arrays;
+
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.apache.log4j.Logger;
 
 
 
 public class CustomLogger implements MethodInterceptor{
 	
-
+	private final Logger log = Logger.getLogger(CustomLogger.class);
 
 	@Override
 	public Object invoke(MethodInvocation method) throws Throwable {
-		System.out.println("BEFORE CGFHVJGFCNH");
+		Object target = method.getThis().getClass();
+		
+		log.info(String.format("test BEFORE %s", target.toString()));
 		Object result = method.proceed();
-		System.out.println("AFTER CGFHVJGFCNH");
+		log.info("testAFTER");
 		return result;
 	}
 
